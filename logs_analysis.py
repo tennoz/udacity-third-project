3b9649ca9a91d6d4113ad1edc73423df839a58de
= <=>
2018-02-05 17:14:46 +0200

    final

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,6 +1,6 @@
 import psycopg2
-
 DBNAME = "news"
+
 
 def execute_query(query):
     """Connects to the database, runs the query passed to it,
@@ -17,11 +17,11 @@ def top_articles():
     '''Returns top 3 most read articles'''
 
     query = """
-        select articles.title as article, count(*) as views 
-        from articles join log 
-        on log.path ~~ concat('%', articles.slug) 
-        where log.status = '200 OK' 
-        group by articles.title 
+        select articles.title as article, count(*) as views
+        from articles join log
+        on log.path ~~ concat('%', articles.slug)
+        where log.status = '200 OK'
+        group by articles.title
         order by views desc limit 3;
         """
 
@@ -30,20 +30,21 @@ def top_articles():
     print('\nTop three articles:')
     for i in output:
         print('"' + i[0] + '"' + ' -- ' + str(i[1]) + ' views')
-#turned i[1] into str in order to concatenate
-#where i[0] is title and i[1] is number of views and so on
+# turned i[1] into str in order to concatenate
+# where i[0] is title and i[1] is number of views and so on
+
 
 def top_authors():
     '''Returns top authors by page views'''
 
     query = """
-        select authors.name as author, count(*) as views 
-        from authors join articles 
-        on authors.id = articles.author 
-        join log 
-        on log.path ~~ concat('%', articles.slug) 
-        where log.status = '200 OK' 
-        group by authors.name 
+        select authors.name as author, count(*) as views
+        from authors join articles
+        on authors.id = articles.author
+        join log
+        on log.path ~~ concat('%', articles.slug)
+        where log.status = '200 OK'
+        group by authors.name
         order by views desc;
     """
 
@@ -52,13 +53,14 @@ def top_authors():
     print('\nTop authors: ')
     for i in output:
         print('"' + i[0] + '"' + ' -- ' + str(i[1]) + ' views')
-        
+
+
 def errors():
     '''Returns days with more than 1% requests lead to error'''
 
     query = """
         select to_char(date, 'Mon DD, YYYY') as date, percent
-        from err_prc 
+        from err_prc
         where percent > 1.0;
         """
 
@@ -67,6 +69,7 @@ def errors():
     print('\nDays with errors > 1%')
     for i in output:
         print(i[0] + ' -- ' + str(i[1]) + '%' + ' errors')
+
 
 top_articles()
 top_authors()
